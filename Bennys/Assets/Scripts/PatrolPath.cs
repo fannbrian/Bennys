b46c57d43aa359fb069d1ff5e1e9884e92ac38cb
Brian Fann <fannbrian@gmail.com>
2018-07-06 23:21:04 -0700

    Added Basic NavMesh

---
+++ Bennys/Assets/Scripts/PatrolPath.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Data structure for a npc's patrol path.
+/// </summary>
+[ExecuteInEditMode]
+public class PatrolPath : MonoBehaviour
+{
+    [Tooltip("Points in the patrol path.")]
+    public Vector3[] points;
+
+    [Tooltip("How big each point appears in the scene view.")]
+    public float pointSize = 0.25f;
+
+    [Tooltip("Color of each point in scene view.")]
+    public Color pointColor = Color.red;
+
+    [Tooltip("Color of path between points in scene view.")]
+    public Color pathColor = Color.red;
+
+    private void OnDrawGizmosSelected()
+    {
+        // Draw a cube at each point
+        foreach(var point in points)
+        {
+            Gizmos.color = pointColor;
+            Gizmos.DrawCube(point, Vector3.one * pointSize);
+        }
+    }
+}
