7d0e8b911e4e2532157a0d5c7adddd9d7ea05326
Pitabreadgamedev <peterdcsulb16@gmail.com>
2018-07-07 02:08:47 -0700

    Pause Menu Functionality. Pause Menu stops time in-game. Menu button loads main menu scene. Quit should quit the application if a build is made using Application.Quit().

---
+++ Bennys/Assets/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;  // public to be accessible from other scripts. static to be easily checked from other scripts. 
+
+    public GameObject pauseMenuUI; // Pause Menu UI game object assigned in inspector
+	
+	// Update is called once per frame
+	void Update ()    
+    {
+		if (Input.GetKeyDown (KeyCode.Escape))
+        {
+            if (GameIsPaused)   // if escape key is pressed and GameIsPaused == true, then game will resume .
+            {
+                Resume();
+            } 
+            else   
+            {
+                Pause();
+            }
+        }
+	}
+
+    public void Resume() // Pause UI set to inactive, time reset to 1, GameIsPaused set to false
+    {
+        pauseMenuUI.SetActive(false); 
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause() // Pause UI set to active, time stopped, GameIsPaused set to true.
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu() // If menu button is pressed, then load Main Menu scene
+    {
+        SceneManager.LoadScene( "Main Menu");
+        Debug.Log("Loading menu... ");
+    }
+
+    public void QuitGame() // if Quit button is pressed, then close application
+    {
+        Application.Quit();
+        Debug.Log("Quitting game...");
+    }
+}
