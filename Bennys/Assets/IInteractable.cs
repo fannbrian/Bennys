77e450d71c70f82eb952071330fb8cdaf996b165
Michael Zatlin <michael.zatlin@yahoo.com>
2018-07-07 02:50:41 -0700

    Player Interactions
    -Implemented player movement
    -player faces direction of movement
    -player uses raycast to touch furniture
    -when the player is close enough and presses the spacebar, the furniture will log that it was touched

---
+++ Bennys/Assets/IInteractable.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This interface will make hooking up interactions to player actions a hell of a lot easier
+public interface IInteractable {
+    void OnInteraction();
+}
---
+++ Bennys/Assets/PlayerInteract.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour {
+    IInteractable interact;
+    RaycastHit2D hit;
+    //This update will draw the raycasthit and check if the player is touching an interactable object
+    private void Update()
+    {
+        hit = Physics2D.Raycast(transform.position, transform.up, 0.3f, LayerMask.GetMask("Furniture"));
+        Debug.DrawRay(transform.position, transform.up);
+
+        if (hit.collider != null)
+        {
+            interact = hit.collider.GetComponent<Collider2D>().GetComponent<IInteractable>();
+
+            //If the player is touching an interactable object and presses the space bar, the object will do the interaction
+            if (interact != null && Input.GetKeyDown(KeyCode.Space))
+            {
+                interact.OnInteraction();
+                interact = null;
+            }
+        }
+    }
+
+    
+
+ 
+}
