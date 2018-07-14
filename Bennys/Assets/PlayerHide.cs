c3fc88d5c42f70c694ed0051082a09ad99aeb331
Michael Zatlin <michael.zatlin@yahoo.com>
2018-07-13 21:01:55 -0700

    Player Hide Mechanic (First Iteration)
    -When the player interacts with the table, and clicks on the Hide button, it will take one second for the player to "vanish" under the table.
    -This will cause the player to be unable to move.
    -Pressing the space bar will cause the player to reappear again after 0.5 seconds.
    -I had issues with the spacebar "Submit" button firing off multiple OnClick events at once so for the time being I disabled it.

---
+++ Bennys/Assets/PlayerHide.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class PlayerHide : MonoBehaviour {
+    private TableGui interaction;
+    public bool ishidden;
+    private Collider2D collide;
+    private SpriteRenderer render;
+    private PlayerController playerfind;
+    private PlayerInteract playerinteract;
+
+    //Grab everything about the player, including the sprite renderer and collider
+    void Start () {
+        interaction = FindObjectOfType<TableGui>();
+        playerfind = interaction.player.GetComponent<PlayerController>();
+        playerinteract = FindObjectOfType<PlayerInteract>();
+        collide = GetComponent<Collider2D>();
+        render = GetComponent<SpriteRenderer>();
+        interaction.OnPrepareToHide += HandleHide;
+	}
+    void Update()
+    {
+     //Check if the player wants to opt out of hiding
+        if (ishidden && Input.GetKeyDown(KeyCode.Space))
+        {
+            HandleUnhide();
+        }
+    }
+    //This sets the necessary components inactive to simulate "hiding"
+        void HandleHide()
+    {
+        ishidden = true;
+        collide.enabled = false;
+        render.enabled = false;
+    }
+    //turns the previously deactivated components back on after 0.5 second delay
+    void HandleUnhide()
+    {
+        StartCoroutine(UnHideDelay());
+    }
+    IEnumerator UnHideDelay()
+    {
+        yield return new WaitForSeconds(0.5f);
+        ishidden = false;
+        collide.enabled = true;
+        render.enabled = true;
+        playerfind.enabled = true;
+        playerinteract.enabled = true;
+    }
+
+}
