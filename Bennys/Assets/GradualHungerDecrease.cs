b112c56659a02fe317e96eeb45f9759135bf01c5
Michael Zatlin <michael.zatlin@yahoo.com>
2018-07-27 18:14:47 -0700

    Added Opening Scene
    When the player clicks on the play button in the main menu, it will take them to the new scene.
    Scene only has basic text with one of the text panels flashing every second
    If the user presses any key in this scene, they will immediately go into Level1
    If not, Level1 will be loaded in 10 seconds

--- Bennys/Assets/GradualHungerDecrease.cs
+++ Bennys/Assets/GradualHungerDecrease.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class GradualHungerDecrease : MonoBehaviour {
     public PlayerHunger hunger;
+            PlayerMoney money;
     float seconds;
 	// Use this for initialization
 	void Start () {
         hunger = FindObjectOfType<PlayerHunger>();
+        money = FindObjectOfType<PlayerMoney>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
         seconds += Time.deltaTime;
-        if(seconds > 2.0f)
+        if(seconds > 2.0f && money.Money > 0)
         {
             hunger.Hunger--;
             seconds = 0;
         }
+
 	}
 
 }
---
+++ Bennys/Assets/PressAnyKey.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PressAnyKey : MonoBehaviour {
+    Canvas canvas;
+    Text text;
+
+	void Start () {
+        canvas = GetComponent<Canvas>();
+        text = canvas.GetComponent<Text>();
+        StartCoroutine(FlashingText());
+    }
+	
+	//checks if the user presses and keys 
+	void Update () {
+      
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("Level1");
+        }
+	}
+
+    //Will infinitely blink the text
+    IEnumerator FlashingText()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            canvas.enabled = false;
+            yield return new WaitForSeconds(1f);
+            canvas.enabled = true;
+        }
+    }
+}
