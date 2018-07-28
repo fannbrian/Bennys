cee363a619d7bed7aae3ae72cc5f246bf00397c4
Michael Zatlin <michael.zatlin@yahoo.com>
2018-07-28 16:15:02 -0700

    Server SpeechManager
    When the server grabs the player, they will loop between an array of lines every 2 seconds

---
+++ Bennys/Assets/ServerControlSpeech.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bennys;
+
+public class ServerControlSpeech : MonoBehaviour {
+    public ServerSpeechBubble speechbubble;
+    public Canvas canvas;
+    bool istalking = false;
+	// Use this for initialization
+	void Start () {
+        speechbubble = FindObjectOfType<ServerSpeechBubble>();
+        canvas = speechbubble.GetComponentInParent<Canvas>();
+        canvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (PlayerManager.s.IsGrabbed && !istalking)
+        {
+            istalking = true;
+            canvas.enabled = true;
+            StartCoroutine(Talk());
+          
+            
+            
+        }
+
+	}
+    IEnumerator Talk()
+    {
+        if (PlayerManager.s.IsGrabbed)
+        {
+            yield return new WaitForSeconds(2f);
+            speechbubble.Generatespeech();
+        }
+        canvas.enabled = false;
+        istalking = false;
+    }
+}
---
+++ Bennys/Assets/ServerSpeechBubble.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ServerSpeechBubble : MonoBehaviour {
+    public static string[] textarray= {"Welcome to Benny's", "WeCoMe To BeNnY’s!!", "Would you like desert ?",
+                                "Take your time!", "Please take your time!", "Hi! Welcome! Welcome!",
+                                "Benny’s is welcome to dessert-- you want dessert?", "Eat, eat, eeeeaaat at Benny’s!" };
+   public Text servertext;
+    int index = 0;
+   void Start()
+    {
+        index = Random.Range(0, textarray.Length - 1);
+    }
+    public void Generatespeech()
+    {
+        index = Random.Range(0, textarray.Length - 1);
+        servertext.text = textarray[index];
+    }
+
+}
