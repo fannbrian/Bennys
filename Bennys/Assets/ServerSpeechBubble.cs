e34f63aab1ad36edbe8bdbb7d866f9045aef0f57
Michael Zatlin <michael.zatlin@yahoo.com>
2018-07-28 22:38:23 -0700

    Added in the Cooks
    The Cooks are in all 3 levels now

--- Bennys/Assets/ServerSpeechBubble.cs
+++ Bennys/Assets/ServerSpeechBubble.cs
@@ -7,7 +7,11 @@ public class ServerSpeechBubble : MonoBehaviour {
     public static string[] textarray= {"Welcome to Benny's", "WeCoMe To BeNnY’s!!", "Would you like desert ?",
                                 "Take your time!", "Please take your time!", "Hi! Welcome! Welcome!",
                                 "Benny’s is welcome to dessert-- you want dessert?", "Eat, eat, eeeeaaat at Benny’s!" };
-   public Text servertext;
+
+    public static string[] textarraycook = {"Let’s get cookin’", "LET'S TURN UP THE HEAT!", "Order up!",
+                                "Order up, right now please.", "Here, let me show you the Benny’s welcome.", "Benny is the true cook, I’m only the knife in its hands.",
+                                "Benny welcomes you out there. Please take a seat and order up.", "Ready to eat already? Little impatient, but that’s okay. We will feed ya…","If you want the cook’s attention, show me your pockets." };
+    public Text servertext;
     int index = 0;
    void Start()
     {
@@ -18,6 +22,11 @@ public class ServerSpeechBubble : MonoBehaviour {
     {
         index = Random.Range(0, textarray.Length - 1);
         servertext.text = textarray[index];
+    }
+    public void GeneratecookSpeech()
+    {
+        index = Random.Range(0, textarray.Length - 1);
+        servertext.text = textarraycook[index];
     }
 
 }
